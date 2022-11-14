abd202e8b8d9053e518fb3d1313125ae0bd528d2
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-11-14 16:25:54 +0530

    created the website header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './app/NavBar/NavBar';
+import Rockets from './app/Rockets/Rockets';
+import Missions from './app/Missions/Missions';
+import MyProfile from './app/MyProfile/MyProfile';
 
-function App() {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/Rockets" element={<Rockets />} />
+      <Route path="/Missions" element={<Missions />} />
+      <Route path="/MyProfile" element={<MyProfile />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
