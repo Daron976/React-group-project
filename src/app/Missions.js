270eb2d9131e474327c2e173326565604cb47be3
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-11-14 20:06:42 +0530

    Created the missions structure and fetch rocket api

--- src/app/Missions.js
+++ src/app/Missions.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions, missionState } from '../../Redux/missionSlice.js';
+import Join from './JoinBtn';
+import Membership from './Membership';
 
 function Missions() {
+  const mission = useSelector(missionState);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  });
+
   return (
-    <div>Missions</div>
+    <div>
+      {mission.length ? (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>Mission</td>
+                <td>description</td>
+                <td>Status</td>
+              </tr>
+            </thead>
+            <tbody>
+              {mission.map((mission) => (
+                <tr key={mission.mission_id}>
+                  <td>{mission.mission_name}</td>
+                  <td>{mission.description}</td>
+                  <td>
+                    <Membership reserved={!!mission.reserved} />
+                  </td>
+                  <td>
+                    <Join
+                      reserved={!!mission.reserved}
+                      id={mission.mission_id}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
+    </div>
   );
 }
 
