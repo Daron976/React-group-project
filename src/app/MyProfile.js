6b4ed88f6700718a7f3789602bfdc062d550dedb
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-11-17 14:06:02 +0530

    added the rocket profile functionality

--- src/app/MyProfile.js
+++ src/app/MyProfile.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const joinedMissions = useSelector((state) => state.missionReducer);
+  const rocketsData = useSelector((state) => state.rocketReducer);
 
   return (
     <section className="profile-container">
@@ -11,12 +12,24 @@ const MyProfile = () => {
         <ul className="mission-list">
           {joinedMissions
             .filter((mission) => mission.reserved === true)
-            .map((mission) => <li className="mission-list-item" key={mission.mission_id}>{mission.mission_name}</li>)}
+            .map((mission) => (
+              <li className="mission-list-item" key={mission.mission_id}>
+                {mission.mission_name}
+              </li>
+            ))}
         </ul>
       </section>
       <section className="reserved-rockets">
-        <h1>Header</h1>
-        <ul className="rocket-list" />
+        <h1>My Rockets</h1>
+        <ul className="rocket-list">
+          {rocketsData
+            .filter((rocket) => rocket.reserved === true)
+            .map((rocket) => (
+              <li className="rocket-list-item" key={rocket.id}>
+                {rocket.rocket_name}
+              </li>
+            ))}
+        </ul>
       </section>
     </section>
   );
