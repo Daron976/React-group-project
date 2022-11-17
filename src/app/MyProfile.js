614088d847509a404aed1ae36e9088d3fe295c28
Daron Wiafe <daronkwiafe@gmail.com>
2022-11-17 10:11:20 +0200

    setup reserved rockets

--- src/app/MyProfile.js
+++ src/app/MyProfile.js
@@ -5,13 +5,19 @@ const MyProfile = () => {
   const joinedMissions = useSelector((state) => state.missionReducer);
 
   return (
-    <section className="join-missions">
-      <h1>My Missions</h1>
-      <ul className="mission-list">
-        {joinedMissions
-          .filter((mission) => mission.reserved === true)
-          .map((mission) => <li className="mission-list-item" key={mission.mission_id}>{mission.mission_name}</li>)}
-      </ul>
+    <section className="profile-container">
+      <section className="join-missions">
+        <h1>My Missions</h1>
+        <ul className="mission-list">
+          {joinedMissions
+            .filter((mission) => mission.reserved === true)
+            .map((mission) => <li className="mission-list-item" key={mission.mission_id}>{mission.mission_name}</li>)}
+        </ul>
+      </section>
+      <section className="reserved-rockets">
+        <h1>Header</h1>
+        <ul className="rocket-list" />
+      </section>
     </section>
   );
 };
