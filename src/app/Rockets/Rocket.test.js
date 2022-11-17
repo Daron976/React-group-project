1c6ab4dd4f149ea5ac0d83bbf7223369c8fc94d5
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-11-17 16:08:46 +0530

    Added the Jest tests for rockets

---
+++ src/app/Rockets/Rocket.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MyProfile from '../MyProfile/MyProfile';
+import Rockets from './Rockets';
+import { store } from '../../redux/configureStore';
+
+describe('Rocket components', () => {
+  it('Render component', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Rockets />
+          </Router>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Testing the Profile component', () => {
+  test('Check rendering of the Profile component', () => {
+    const { getByText } = render(
+      <Router>
+        <Provider store={store}>
+          <MyProfile />
+        </Provider>
+      </Router>,
+    );
+    expect(getByText('My Rockets')).toBeInTheDocument();
+  });
+});
